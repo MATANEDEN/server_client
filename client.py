baf6bbcc72a8a4ff6c595e5593b0bf3672ee2404
MATANEDEN <73553305+MATANEDEN@users.noreply.github.com>
2022-12-27 16:31:01 +0200

    add break  to terminate while execution

--- client.py
+++ client.py
@@ -9,14 +9,16 @@ def main():
     print("setting up server....")
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     my_socket.connect((SERVER_IP, SERVER_PORT))
-    connection = my_socket.recv(MAX_MSG_LEGTH).decode('utf-8')
-    print(connection)
+    data = my_socket.recv(MAX_MSG_LEGTH).decode('utf-8')
+    print(data)
 
     while True:
-        message = input("print message")
+        message = input("print message or EXIT to finish")
         my_socket.send(message.encode('utf-8'))
-        data = my_socket.recv(MAX_MSG_LEGTH).decode()
-        print(data)
+        client_data = my_socket.recv(MAX_MSG_LEGTH).decode()
+        print("Server send back " + client_data)
+        if message == "EXIT":
+            break
     my_socket.close()
 
 
